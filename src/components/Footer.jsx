fa8745f55e2068cc565e668a9f553daf6dfe6cab
Om Patel <omupatel7113@gmail.com>
2023-07-01 13:54:03 +0530

    Initial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h1>TechyStar</h1>
+                <p>@All Rights Reserved.</p>
+            </div>
+
+            <div>
+                <h4>Follow Us</h4>
+                <div>
+                    <a href='https://instgram.com'>Instagram</a>
+                    <a href='https://github.com'>GitHub</a>
+                    <a href='https://linkedin.com'>LinkedIn</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
